c172c5d5b068c0c740eed83d6aa0e912c99d2744
Scott Doggett <sdoggett@lcs.on.ca>
2020-05-06 18:15:45 -0400

    buttons change colour when answer is right or wrong

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -45,9 +45,9 @@ class ViewController: UIViewController {
         let actualAnswer = quiz[questionNumber].answer
         
         if userAnswer == actualAnswer {
-            print("right")
+            sender.backgroundColor = UIColor.green
         } else {
-            print("wrong")
+            sender.backgroundColor = UIColor.red
         }
         
         if questionNumber + 1 < quiz.count {
@@ -56,11 +56,15 @@ class ViewController: UIViewController {
             questionNumber = 0
         }
         
-        updateUI()
-    }
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+        
+        }
     
-    func updateUI() {
+    @objc func updateUI() {
         questionLabel.text = quiz[questionNumber].text
+        falseButton.backgroundColor = UIColor.clear
+        trueButton.backgroundColor = UIColor.clear
+        progressBar.progress = Float(questionNumber + 1) / Float(quiz.count)
     }
 
 }
